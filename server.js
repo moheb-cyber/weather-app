a05776582d3c675b3e83e6368a08c2c6965752d7
Reza <mohebhosseinifard@email.com>
2026-08-19 14:51:36 +0330

    Create Express backend server

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+    res.send("Weather API Server is running");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
